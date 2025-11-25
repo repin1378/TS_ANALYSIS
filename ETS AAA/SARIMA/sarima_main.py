4542e149c2bed7a8319c7ef25f89515a7bcf675e
repin1378 <repin1378@gmail.com>
2025-11-25 03:26:17 +0300

    Добавлены модули для SARIMA: 6. Преобразование Бокса-Кокса 7. Дифференцирование 8. Удаление смещения

--- ETS AAA/SARIMA/sarima_main.py
+++ ETS AAA/SARIMA/sarima_main.py
@@ -17,6 +17,14 @@ from generation_config import PARAMS, ROADS
 from SARIMA.outliers import process_outliers
 # Нормализация масштаба
 from SARIMA.scale import scale_series
+# Преобразование Бокса-Кокса
+from SARIMA.boxcox_transform import boxcox_transform
+# Дифференцирование
+from SARIMA.differencing import differencing
+# Удаление смещения
+from SARIMA.bias_removal import remove_bias
+
+
 
 
 
@@ -29,6 +37,9 @@ def sarima_test_run():
     3. Выполняет шаг 1 (проверку входного ряда)
     4. Обработка выбросов
     5. Нормализация масштаба
+    6. Преобразование Бокса-Кокса
+    7. Дифференцирование
+    8. Удаление смещения
     """
 
     road = ROADS[0]                                                 # Например, "Окт"
@@ -46,7 +57,7 @@ def sarima_test_run():
     # Шаг 1 — проверка входных данных
     assert_valid_series(x)
 
-    print("\nПроверка пройдена успешно!")
+    print("Проверка пройдена успешно!")
     print("Можно переходить к шагу 2 (обработка выбросов).")
 
     clean_series, outlier_flags, num_outliers = process_outliers(x)
@@ -60,6 +71,31 @@ def sarima_test_run():
 
     print("Среднее значение u1:", mean_value)
     print("Первые 5 значений после нормализации:", scaled_series[:5])
+
+    boxcox_series, geometric_mean = boxcox_transform(
+        scaled_series,
+        lambda_value=1
+    )
+    print("GM:", geometric_mean)
+    print("Первые 5 значений Box–Cox:", boxcox_series[:5])
+
+    print("Шаг 5 — дифференцирование (Icase=1)")
+    diff_series, intermediate, nd = differencing(boxcox_series, icase=1)
+
+    print("nd =", nd)
+    print("Первые 10 значений x^(5):", diff_series[:5])
+
+    # Шаг 8 — удаление смещения
+    bias_removed_series, mean_u5 = remove_bias(
+        diff_series,
+        icase=1,  # тот же Icase, что и при дифференцировании !!!
+        nd=nd
+    )
+
+    print("Шаг 8 — удаление смещения")
+    print("u5 =", mean_u5)
+    print("Первые 5 bias_removed_series:", bias_removed_series[:5])
+
 
 if __name__ == "__main__":
     sarima_test_run()
